6b814b8d37facc7c291dc841383007cea80982a9
IamElaya <asison13@gmail.com>
2016-02-11 03:19:40 +0000

    added bootstrap

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,11 +1,11 @@
 class CityController < ApplicationController
 
-    def show
-    contact = Contact.find_by(id: params[:id])
-    if contact
-      respond_with :api, contact # contact.to_json
-    else
-      respond_with(:api, {status: "404", message: "Could not find contact with id " + params[:id]})
-    end
-  end
+  #   def show
+  #   contact = Contact.find_by(id: params[:id])
+  #   if contact
+  #     respond_with :api, contact # contact.to_json
+  #   else
+  #     respond_with(:api, {status: "404", message: "Could not find contact with id " + params[:id]})
+  #   end
+  # end
 end

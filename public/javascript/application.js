4909d3f9250e0291c9bc6c1ce9c5839414d9adc9
IamElaya <asison13@gmail.com>
2016-02-11 03:02:11 +0000

    It works now

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,59 +1,50 @@
 $(function() {
-  $("#getWeather").on('click', function() {
-    console.log("#getWeather")
+  
+    // $("#findCity").on('submit', function() { 
+  $("#findCity").on('click', function() {
+    var i;
+    var cityname;
+    var cityzmw;
+    var arr=[];
+    var value = $("#autocomplete").val();  
     $.ajax({
-    url: "http://api.wunderground.com/api/383901c8559cb3ce/geolookup/conditions/q/Canada/Vancouver.json",
-    dataType: "json",
-    success: function(parsed_json) {
-    var location = parsed_json['location']['city'];
-    var temp_f = parsed_json['current_observation']['temp_f'];
-    alert("Current temperature in " + location + " is: " + temp_f);
-    }
-  });
-  });
-    // $("#findCity").on('submit', function() {
-      var i;
-      var out;
-      var arr=[];
-    $("#findCity").on('click', function() {
-      var value = $("#autocomplete").val();  
+      url:"http://autocomplete.wunderground.com/aq?&cb=call=?",
+      dataType: "json",
+      data:{
+        "query":value
+      },
+      crossDomain: true,
+      success: function (json_for_cities) {
+        var table = $("#cities").find('tbody').empty();
+        json_for_cities.RESULTS.forEach(function(city){
+          var cityname = city.name;
+          var cityzmw = city.zmw;
+          var tr = $("<tr>").appendTo(table);
+          tr.html('<td class="city"><a href="#">'+ cityname +'</a></td>');
+          tr.on('click', function() {
+            var url = "http://api.wunderground.com/api/383901c8559cb3ce/forecast/q/zmw:"+cityzmw+".json"
+            $.ajax({
+              url: url,
+              dataType: 'json',
+              success: function(json_for_city) {
+                var high = json_for_city.forecast.simpleforecast.forecastday[0].high.celsius;
+                var low = json_for_city.forecast.simpleforecast.forecastday[0].low.celsius;
+                var condition = json_for_city.forecast.simpleforecast.forecastday[0].conditions;
+                alert("High temperature of " + high + ", low temperature of " + low + ", a condition of " + condition +".");
+              }
 
-    $.ajax({
-    url:"http://autocomplete.wunderground.com/aq?&c=US&cb=call=?",
-    dataType: "json",
-    data:{
-    "query":value
-    },
-    crossDomain: true,
-
-    success: function (parsed_json) {
-    var c =$.each(parsed_json.RESULTS,function(i,item){
-    out=(parsed_json.RESULTS[i].name);
-     arr.push(out);
-     var table = $("#cities").find('tbody').empty();
-     arr.forEach(function(city) {
-      var tr = $("<tr>").attr('href').addClass('city').appendTo(table);
-      $("<td>").appendTo(tr).text(city);
-      });
-     });
-    console.log(c)
-
-     // });
-    //  $("#autocomplete").autocomplete({
-    //  source:arr
-
-    // });
-
-    },
-     error: function (xhr, ajaxOptions, thrownError) {
-    alert(xhr.status);
-    alert(thrownError);
+            });
+            return false;
+          });
+        });
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        alert(xhr.status);
+        alert(thrownError);
    
-        }
-
+      }
     }); 
   });
- // });
 
 });
 
